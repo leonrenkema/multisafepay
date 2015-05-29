1d10bb2681f2cc64d30898750b343ca5f232c0a0
Ruud Jonk <ruud@multisafepay.com>
2015-05-29 15:46:48 +0200

    Fixes #2. Fixe typo that was copied to other objects.

--- src/MultiSafepay/API/Object/Issuers.php
+++ src/MultiSafepay/API/Object/Issuers.php
@@ -7,7 +7,7 @@ class MultiSafepay_API_Object_Issuers extends MultiSafepay_API_Object_Core {
     public function get(){
         
         $result = parent::get('issuers', 'ideal');
-        $this->success= $result->succes;
+        $this->success= $result->success;
         $this->data= $result->data;
         
         return $this->data;
--- src/MultiSafepay/API/Object/Transactions.php
+++ src/MultiSafepay/API/Object/Transactions.php
@@ -6,14 +6,14 @@ class MultiSafepay_API_Object_Transactions extends MultiSafepay_API_Object_Core{
     
     public function patch($body, $endpoint) {
         $result = parent::patch(json_encode($body), $endpoint);
-        $this->success = $result->succes;
+        $this->success = $result->success;
         $this->data = $result->data;
         return $this->data;
     }
     
     public function get($id) {
         $result = parent::get('transactions', $id);
-        $this->success = $result->succes;
+        $this->success = $result->success;
         $this->data = $result->data;
         return $this->data;
     }
